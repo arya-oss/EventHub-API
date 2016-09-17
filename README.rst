89eed993df2b2324da19d99980b3dc6555b5465d
Rajmani Arya <rajmani1995@gmail.com>
2016-09-17 14:25:52 +0530

    adding base files

---
+++ README.rst
@@ -0,0 +1,164 @@
+## EventHub RESTful API
+
+### HOW TO SETUP
+```
+git clone http://github.com/rajmani1995/EventHub-API.git
+
+pip install -r requirements.txt
+
+python app.py
+
+```
+### To Manage migrations
+
+```
+# 
+flask db init
+# to create a new migration
+flask db migrate
+# to upgrade database
+flask db upgrade
+```
+server started on http://127.0.0.1:5000
+
+```
+ROOT_URL = http://127.0.0.1:5000/api/v1
+```
+
+### HOW TO
+
+#### To create a new user
+```
+$ curl -i -X POST -H "Content-Type:application/json" http://localhost:5000/api/v1/users -d '{"username": "rest_user","first_name": "Rest","password":"s3cr3t","email":"restuser@gmail.com","last_name":"User","phone":"9000012345"}'
+
+HTTP/1.0 201 CREATED
+Content-Type: application/json
+Location: http://localhost:5000/api/v1/users/7
+Content-Length: 28
+Server: Werkzeug/0.11.11 Python/2.7.12
+Date: Sat, 17 Sep 2016 08:10:46 GMT
+
+{
+  "username": "rest_user"
+}
+```
+#### To get list of all user
+```
+$ curl -u rest_user:s3cr3t -X GET http://localhost:5000/api/v1/users
+
+HTTP/1.0 200 OK
+Content-Type: application/json
+Content-Length: 102
+Server: Werkzeug/0.11.11 Python/2.7.12
+Date: Sat, 17 Sep 2016 08:10:02 GMT
+
+{
+  "count": 1, 
+  "status": "success", 
+  "users": [
+    {
+      "_id": 1,
+      "email": "restuser@gmail.com", 
+      "full_name": "Rest User",
+      "phone": "9000012345",
+      "email": "restuser@gmail.com"
+    }
+  ]
+}
+```
+#### To get a particular user
+```
+$ curl -u rest_user:s3cr3t -X GET  http://localhost:5000/api/v1/users/1
+
+HTTP/1.0 200 OK
+Content-Type: application/json
+Content-Length: 71
+Server: Werkzeug/0.11.11 Python/2.7.12
+Date: Sat, 17 Sep 2016 08:20:51 GMT
+
+{
+  "email": "restuser@gmail.com", 
+  "full_name": "Rest User"
+  "phone": "9000012345",
+  "email": "restuser@gmail.com"
+}
+
+```
+#### One time feedback submit
+
+```
+$ curl -u rest_user:s3cr3t -i -X POST -H "Content-Type:application/json" http://localhost:5000/api/v1/feedback -d '{"stars":"1", "comment":"Noce Job !"}'
+
+HTTP/1.0 200 OK
+Content-Type: application/json
+Content-Length: 64
+Server: Werkzeug/0.11.11 Python/2.7.12
+Date: Sat, 17 Sep 2016 08:27:33 GMT
+
+{
+  "message": "Feedback Submitted !", 
+  "status": "success"
+}
+
+
+```
+
+#### To get list of all events
+```
+# when {0,1,2} , 0 -> past events, 1->today events and 2 -> upcoming events
+
+$ curl -u prakash:s3cr3t -i -X GET http://localhost:5000/api/v1/events?when=0
+
+HTTP/1.0 200 OK
+Content-Type: application/json
+Content-Length: 318
+Server: Werkzeug/0.11.11 Python/2.7.12
+Date: Sat, 17 Sep 2016 08:31:35 GMT
+
+{
+  "events": [
+    {
+      "_id": 1,
+      "contact": "9000012345", 
+	    "contact_alt": "9000012345", 
+      "location": "Stadium", 
+      "logo_url": "/assets/ganesh_pooja.png", 
+      "requirements": "", 
+      "schedule": "Thu, 08 Sep 2016 20:00:00 GMT", 
+      "title": "Ganesh Pooja"
+    }
+  ], 
+  
+  "status": "success"
+}
+
+```
+
+#### To get a particular event
+
+```
+$ curl -urest_user:s3cr3t -i -X GET http://localhost:5000/api/v1/events/1
+
+HTTP/1.0 200 OK
+Content-Type: application/json
+Content-Length: 295
+Server: Werkzeug/0.11.11 Python/2.7.12
+Date: Sat, 17 Sep 2016 08:31:11 GMT
+
+{
+  "event": {
+    "_id": 2,
+    "contact": "9000012345", 
+    "contact_alt": "9000012345", 
+    "location": "NIT Warangal", 
+    "logo_url": "/assets/hackathon.png", 
+    "requirements": "Laptop", 
+    "schedule": "Sun, 18 Sep 2016 20:00:00 GMT", 
+    "title": "Hackathon"
+  }, 
+  "status": "success"
+}
+
+```
+
+### More updates coming soon
---
+++ app.py
@@ -0,0 +1,239 @@
+#!/usr/bin/env python
+
+import os
+import bcrypt
+import re
+
+from datetime import datetime, date
+
+from flask import Flask, abort, request, json, jsonify, g, url_for
+from flask_sqlalchemy import SQLAlchemy
+from sqlalchemy.sql import func
+from flask_httpauth import HTTPBasicAuth
+from flask_migrate import Migrate
+
+# initialization
+
+app = Flask(__name__)
+
+app.config['SECRET_KEY'] = 'somerandomstring'
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///db.sqlite3'
+app.config['SQLALCHEMY_COMMIT_ON_TEARDOWN'] = True
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = True
+
+# extensions
+
+db = SQLAlchemy(app)
+auth = HTTPBasicAuth()
+migrate = Migrate(app, db)
+
+class User(db.Model):
+
+    __tablename__ = 'user'
+    _id = db.Column(db.Integer, primary_key=True)
+    username = db.Column(db.String(32), index=True, unique=True)
+    password = db.Column(db.String(64))
+    first_name = db.Column(db.String(16))
+    last_name = db.Column(db.String(16))
+    email = db.Column(db.String(32), unique=True)
+    phone = db.Column(db.String(13))
+    date_joined = db.Column(db.DateTime, default=datetime.utcnow)
+    feedback = db.relationship('Feedback', backref='User', uselist=False)
+
+    def __init__(self, username, first_name, email, last_name, phone):
+        self.username = username
+        self.email = email
+        self.first_name = first_name
+        self.last_name = last_name
+        self.phone = phone
+    
+    def hash_password(self, password):
+        self.password = bcrypt.hashpw(password, bcrypt.gensalt())
+
+    def check_password(self, password):
+        return bcrypt.checkpw(password, str(self.password))
+
+    def full_name(self):
+        return self.first_name + ' ' + self.last_name
+
+    def __repr__(self):
+        print '<User %r>' % self.username
+
+class Feedback(db.Model):
+    __tablename__='feedback'
+    _id = db.Column(db.Integer, primary_key=True)
+    stars = db.Column(db.Integer)
+    comment = db.Column(db.String(50))
+    ts_submit = db.Column(db.DateTime, default=datetime.utcnow())
+    user_id = db.Column(db.Integer, db.ForeignKey('user._id'))
+
+    def __init__(self, stars, comment):
+        self.stars = stars
+        self.comment = comment
+
+    def __repr__(self):
+        print '<Feedback %r>'%self.comment
+
+event_user = db.Table('event_user',
+    db.Column('event_id', db.Integer, db.ForeignKey('event._id')),
+    db.Column('user_id', db.Integer, db.ForeignKey('user._id')),
+    db.Column('interested', db.Boolean)
+)
+
+class Event(db.Model):
+    __tablename__='event'
+    _id = db.Column(db.Integer, primary_key=True)
+    title = db.Column(db.String(32))
+    location = db.Column(db.String(32))
+    schedule = db.Column(db.DateTime)
+    logo_url = db.Column(db.String(128), default="assets/logo.png")
+    requirements = db.Column(db.String(64))
+    refreshment = db.Column(db.Boolean)
+    contact = db.Column(db.String(13))
+    contact_alt = db.Column(db.String(12))
+    event_user = db.relationship(
+        'User',
+        secondary=event_user,
+        backref=db.backref('events', lazy='dynamic')
+    )
+
+    def __init__(self, title, location, schedule, contact):
+        self.title = title
+        self.location= location
+        self.schedule = schedule
+        self.contact = contact
+
+    def to_json(self):
+        return {
+            '_id' : self._id,
+            'title': self.title, 
+            'location': self.location, 
+            'schedule': self.schedule, 
+            'logo_url': self.logo_url, 
+            'requirements': self.requirements,
+            'contact' : self.contact,
+            'contact_alt' : self.contact_alt
+        }
+
+    def __repr__(self):
+        print '<Event %r>'%self.title
+
+def custom_error(status_code, err_msg):
+    response = {
+        'status': status_code,
+        'message': err_msg
+    }
+    return jsonify(response)
+
+@auth.verify_password
+def verify_password(username, password):
+    user = User.query.filter_by(username=str(username)).first()
+    if not user or not user.check_password(str(password)):
+        return False
+    g.user = user
+    return True
+
+@app.route('/api/v1/users', methods=['POST'])
+def new_user():
+    username = str(request.json.get('username'))
+    password = str(request.json.get('password'))
+    first_name = str(request.json.get('first_name'))
+    last_name = str(request.json.get('last_name'))
+    email = str(request.json.get('email'))
+    phone = str(request.json.get('phone'))
+    
+    if re.match(r"^[a-zA-Z0-9_]+$", username) is None:
+        return custom_error(400, 'username format is lowercase and numeric')
+    if first_name is None or len(first_name) < 3:
+        return custom_error(400, 'first_name cannot be null')
+    if re.match(r"[^@]+@[^@]+\.[^@]+", email) is None:
+        return custom_error(400, 'email is not correct')
+    if password is None or len(password) < 6:
+        return custom_error(400, 'password length must be > 6')
+    user = User.query.filter_by(username=username).first()
+    if user is not None:
+        return custom_error(409, 'username already exists ! try differenr')
+    
+    user = User(username, first_name, email, last_name, phone)
+    user.hash_password(password)
+
+    db.session.add(user)
+    db.session.commit()
+
+    return (jsonify({'username': user.username}), 201, {'Location': url_for('get_user', _id=user._id, _external=True)})
+
+@app.route('/api/v1/users/<int:_id>', methods=['GET'])
+def get_user(_id):
+    user = User.query.get(_id)
+    if not user:
+        custom_error(404, 'User not found')
+    return jsonify({'full_name': user.full_name(), 'email': user.email})
+
+@app.route('/api/v1/users', methods=['GET'])
+@auth.login_required
+def get_all_user():
+    users = User.query.all()
+    if not users:
+        return jsonify({'status':'success', 'count':0})
+    else:
+        res = {'status':'success', 'count': len(users), 'users': []}
+        for user in users:
+            res['users'].append({'_id': user._id,'full_name': user.full_name(), 'email': user.email})
+
+        return jsonify(res)
+
+@app.route('/api/v1/feedback', methods=['POST'])
+@auth.login_required
+def submit_feedback():
+    stars = int(request.json.get('stars'))
+    comment = str(request.json.get('comment'))
+    f = Feedback.query.filter_by(user_id=g.user._id).first()
+    if f is not None:
+        return jsonify({'status':'error', 'message':'Feedback Already Submitted !'})
+
+    feedback = Feedback(stars, comment)
+    feedback.user_id = g.user._id
+    db.session.add(feedback)
+    db.session.commit()
+
+    return jsonify({'status':'success', 'message':'Feedback Submitted !'})
+
+@app.route('/api/v1/events/<int:_id>', methods=['GET'])
+@auth.login_required
+def get_event(_id):
+    event = Event.query.get(_id)
+
+    if event is None:
+        return custom_error(404, 'Event not found')
+
+    return jsonify({'status': 'success', 'event': event.to_json()})
+
+@app.route('/api/v1/events', methods=['GET'])
+@auth.login_required
+def get_all_event():
+    when = int(request.args.get('when'))
+    if when == 0:
+        events = Event.query.filter(func.date(Event.schedule) < date.today())
+    elif when == 1:
+        events = Event.query.filter(func.date(Event.schedule) == date.today())
+    elif when == 2:
+        events = Event.query.filter(func.date(Event.schedule) > date.today())
+    else:
+        return custom_error(400, 'Invalid query body')
+
+    if events is None:
+        return custom_error(404, 'Event not found')
+
+    response = {'status': 'success', 'events': []}
+    for event in events:
+        response['events'].append(event.to_json())
+    return jsonify(response)
+
+
+if __name__=='__main__':
+    if not os.path.exists('db.sqlite3'):
+        db.create_all()
+
+    app.run(debug=True)
+
+
